710c6b0c7d3a1418891e697d990d457c479125da
Sinchana <sinchu1729@gmail.com>
2022-03-26 09:35:30 +0530

    config files

---
+++ lib/configs/palette.dart
@@ -0,0 +1,12 @@
+/* add color constants here */
+
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const orange = Color(0xffFDAB5D);
+  static const blue = Color(0xff034088);
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const green = Color(0xff7CBF25);
+  static const gold = Color(0xffCB925B);
+}
---
+++ lib/configs/text_styles.dart
@@ -0,0 +1,1 @@
+/* add const textsyles here */
